172054d3ee25df5023b026b4474593f2adfb7632
LGZhsss <lgzhss.cs@gmail.com>
2026-03-23 00:04:48 +0800

    chore: 项目深度治理与 .gitignore 规范化 (IDE 分区隔离 + 100+ 冗余文件清理)

--- fix.js
+++
@@ -1,5 +0,0 @@
-// If we want to avoid interpolation in db.prepare, how do we write an update?
-// We can't easily avoid it if the update is dynamic, unless we use string concatenation (which might also trigger it) or just live with it.
-// BUT wait... what if we do:
-// const sql = "UPDATE scheduled_tasks SET " + fields.join(", ") + " WHERE id = ?";
-// const stmt = db.prepare(sql);
--- fix_typecheck2.ts
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-let content = fs.readFileSync('src/db.ts', 'utf8');
-content = content.replace("('UPDATE scheduled_tasks SET ' + fields.join(', ') + ' WHERE id = ?'),", '`UPDATE scheduled_tasks SET ${fields.join(", ")} WHERE id = ?`,');
-fs.writeFileSync('src/db.ts', content);
